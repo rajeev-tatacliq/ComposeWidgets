478c34f14e73b656d01e79fb175a085362c6814c
rajeev-tatacliq <rranjan1@tataunistore.com>
2022-09-28 20:56:54 +0530

    version upgrade

--- app/src/main/java/com/horizonlabs/composewidgets/SplashActivity.kt
+++ app/src/main/java/com/horizonlabs/composewidgets/SplashActivity.kt
@@ -2,10 +2,13 @@ package com.horizonlabs.composewidgets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.horizonlabs.customimageview.TestClass
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Log.d("",TestClass.messages[0])
     }
 }
